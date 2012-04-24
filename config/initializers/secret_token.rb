4b8adced63d75acf23683b9878772eb582d8408f
Syed Haroon <syed.haroon.009@gmail.com>
2012-04-24 08:08:39 +0530

    first commit with basic setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rose::Application.config.secret_token = '0c9ee53d0f421c180da6119b1af1b2ef0aca9c507fe34e80e3c4b39d5f1748c54ddd3d06a5cc142c61a9a006f4b8f6012b30d8d2453491db23fb8634fcecb198'
