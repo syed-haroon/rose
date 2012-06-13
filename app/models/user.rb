bb7d2949598e1e71b0503ae3be5aeee974fb25d5
Syed Haroon <syed.haroon.009@gmail.com>
2012-06-13 23:54:59 +0530

    b4 adding password filed 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :first_name, :last_name,
+  validates :first_name, :last_name, :title_gender,
             :presence => true
 
   has_and_belongs_to_many :qualifications
